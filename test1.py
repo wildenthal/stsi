df2a33da880f3445cef0265a0b68a263ef216c08
wildenthal <8503469+wildenthal@users.noreply.github.com>
2022-11-10 15:16:35 +0100

    actually fixes indentation

--- test1.py
+++ test1.py
@@ -12,15 +12,15 @@ def get_iter(c:complex, thresh:int =4, max_steps:int =25) -> int:
     return i
     
 def plotter(n, thresh, max_steps=25):
-mx = 2.48 / (n-1)
-my = 2.26 / (n-1)
-mapper = lambda x,y: (mx*x - 2, my*y - 1.13)
-img=np.full((n,n), 255)
-for x in range(n):
-    for y in range(n):
-        it = get_iter(complex(*mapper(x,y)), thresh=thresh, max_steps=max_steps)
-        img[y][x] = 255 - it
-return img
+    mx = 2.48 / (n-1)
+    my = 2.26 / (n-1)
+    mapper = lambda x,y: (mx*x - 2, my*y - 1.13)
+    img=np.full((n,n), 255)
+    for x in range(n):
+        for y in range(n):
+            it = get_iter(complex(*mapper(x,y)), thresh=thresh, max_steps=max_steps)
+            img[y][x] = 255 - it
+    return img
     
 n=1000
 img = plotter(n, thresh=4, max_steps=50)
